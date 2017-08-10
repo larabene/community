20ba5bc4472407be19bbacf783d1e33a6c571630
Joshua de Gier <info@pendo.nl>
2017-08-10 18:50:36 +0200

    Renamed UserRequest to MemberRequest

--- app/Http/Requests/MemberRequest.php
+++ app/Http/Requests/MemberRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Member;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class MemberRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
