e0ac621bfeae15edc1a7201a964e887e1c06eae1
Joshua de Gier <info@pendo.nl>
2017-08-15 22:09:39 +0200

    Replaced is_null() by empty() in show.blade.php

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -13,7 +13,7 @@
                     <div class="bg-picture card-box">
                         <div class="profile-info-name">
 
-                            @if(!is_null($profile->logo))
+                            @if(!empty($profile->logo))
                             <img src="{{ asset('storage/uploads/logos/' . $profile->logo) }}" class="img-thumbnail" alt="{{ $profile->name }}">
                             @endif
 
@@ -31,25 +31,25 @@
                                 @markdown($profile->about)
 
                                 <div class="button-list m-t-20">
-                                    @if(!is_null($profile->facebook))
+                                    @if(!empty($profile->facebook))
                                         <a href="{{ $profile->facebook }}" class="btn btn-facebook btn-sm waves-effect waves-light">
                                             <i class="fa fa-facebook"></i>
                                         </a>
                                     @endif
 
-                                    @if(!is_null($profile->twitter))
+                                    @if(!empty($profile->twitter))
                                         <a href="{{ $profile->twitter }}" class="btn btn-twitter btn-sm waves-effect waves-light">
                                             <i class="fa fa-twitter"></i>
                                         </a>
                                     @endif
 
-                                    @if(!is_null($profile->linkedin))
+                                    @if(!empty($profile->linkedin))
                                         <a href="{{ $profile->linkedin }}" class="btn btn-linkedin btn-sm waves-effect waves-light">
                                             <i class="fa fa-linkedin"></i>
                                         </a>
                                     @endif
 
-                                    @if(!is_null($profile->googleplus))
+                                    @if(!empty($profile->googleplus))
                                         <a href="{{ $profile->googleplus }}" class="btn btn-googleplus btn-sm waves-effect waves-light">
                                             <i class="fa fa-google-plus"></i>
                                         </a>
@@ -69,13 +69,13 @@
                         <h4 class="header-title m-t-0 m-b-30">Adres</h4>
 
                         <div class="text-left">
-                            @if(!is_null($profile->address))
+                            @if(!empty($profile->address))
                                 <p class="text-muted font-13"><strong>Adres :</strong> <span class="m-l-15">{{ $profile->address }}</span></p>
                             @endif
-                            @if(!is_null($profile->zipcode) || !is_null($profile->city))
+                            @if(!empty($profile->zipcode) || !empty($profile->city))
                                 <p class="text-muted font-13"><strong>Postcode :</strong> <span class="m-l-15">{{ $profile->zipcode or '' }} {{ $profile->city or '' }}</span></p>
                             @endif
-                            @if(!is_null($profile->country))
+                            @if(!empty($profile->country))
                                 <p class="text-muted font-13"><strong>Land :</strong> <span class="m-l-15">{{ $profile->country }}</span></p>
                             @endif
                         </div>
@@ -92,22 +92,22 @@
                         <h4 class="header-title m-t-0 m-b-30">Contact</h4>
 
                         <div class="text-left">
-                            @if(!is_null($profile->website))
+                            @if(!empty($profile->website))
                                 <p class="text-muted font-13"><strong>Website :</strong> <span class="m-l-15"><a target="_blank" href="{{ $profile->website }}">{{ $profile->website }}</a></span></p>
                             @endif
-                            @if(!is_null($profile->telephone))
+                            @if(!empty($profile->telephone))
                                 <p class="text-muted font-13"><strong>Telefoon :</strong> <span class="m-l-15">{{ $profile->telephone }}</span></p>
                             @endif
-                            @if(!is_null($profile->mobile))
+                            @if(!empty($profile->mobile))
                                 <p class="text-muted font-13"><strong>Mobiel :</strong> <span class="m-l-15">{{ $profile->mobile }}</span></p>
                             @endif
-                            @if(!is_null($profile->whatsapp))
+                            @if(!empty($profile->whatsapp))
                                 <p class="text-muted font-13"><strong>Whatsapp :</strong> <span class="m-l-15">{{ $profile->whatsapp }}</span></p>
                             @endif
-                            @if(!is_null($profile->company_number))
+                            @if(!empty($profile->company_number))
                                 <p class="text-muted font-13"><strong>KvK nummer :</strong> <span class="m-l-15">{{ $profile->company_number }}</span></p>
                             @endif
-                            @if(!is_null($profile->vat_number))
+                            @if(!empty($profile->vat_number))
                                 <p class="text-muted font-13"><strong>BTW nummer :</strong> <span class="m-l-15">{{ $profile->vat_number }}</span></p>
                             @endif
                         </div>
