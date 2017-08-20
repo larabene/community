0b5e8812cb17dfbf2c8360492f0e11097d2dde2d
PendoNL <info@pendo.nl>
2017-08-20 20:39:18 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -119,7 +119,7 @@ class ProfileController extends Controller
             'user_id'       => Auth::user()->id,
             'logo'          => $this->uploadLogo(),
             'founded_at'    => $request->founded_at ? Carbon::createFromFormat('Y', $request->founded_at) : null,
-            'hourly_rate'   => $request->hourly_rate ? floatval(str_replace([' ', ','], ['', '.'], $request->hourly_rate)): null,
+            'hourly_rate'   => $request->hourly_rate ? floatval(str_replace([' ', ','], ['', '.'], $request->hourly_rate)) : null,
         ]));
 
         if (Auth::user()->profiles()->count() === 1) {
@@ -168,7 +168,7 @@ class ProfileController extends Controller
         $profile->update(array_merge($request->getValidInput(), [
             'logo'          => $this->uploadLogo($profile->logo),
             'founded_at'    => $request->founded_at ? Carbon::createFromFormat('Y', $request->founded_at) : null,
-            'hourly_rate'   => $request->hourly_rate ? floatval(str_replace([' ', ','], ['', '.'], $request->hourly_rate)): null,
+            'hourly_rate'   => $request->hourly_rate ? floatval(str_replace([' ', ','], ['', '.'], $request->hourly_rate)) : null,
         ]));
 
         flash('Het bedrijfsprofiel is bijgewerkt', 'success');
