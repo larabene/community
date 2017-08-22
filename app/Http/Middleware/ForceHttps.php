10cbcc9595cb252e3b9861456f04e79dbdd3283c
Joshua de Gier <info@pendo.nl>
2017-08-22 12:52:49 +0200

    Added forceHttps middleware

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App;
+use Closure;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && App::environment('production') && config('app.force_ssl')) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
