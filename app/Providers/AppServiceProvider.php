6cd5cd7381bcf581df4ca87da9675f4722d61902
Roj Vroemen <me@rojvroemen.com>
2017-09-05 21:20:59 +0200

    Move helpers.php to app/ and move validation rules to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Profile;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,15 +17,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        require_once app_path().'/Http/helpers.php';
-        require_once app_path().'/Http/validation.php';
-
         // MySQL Fix for keylength
         Schema::defaultStringLength(191);
 
         Relation::morphMap([
             'profile' => Profile::class,
         ]);
+
+        // Validato Latitude rule
+        Validator::extend('lat', function ($attribute, $value, $parameters, $validator) {
+            return isValidLatitude($value);
+        });
+
+        // Validate Longitude rule
+        Validator::extend('lng', function ($attribute, $value, $parameters, $validator) {
+            return isValidLongitude($value);
+        });
     }
 
     /**
